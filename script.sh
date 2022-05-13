7750f39b56d8bf1bbe489eab4a9ea090abbb9fd6
TheYeetster <64783847+Yeetster-cmd@users.noreply.github.com>
2022-05-13 17:35:03 +0300

    Added full automation (except auto-node_id finding)
    I've finally been able to figure out how to automatically get, format, and put the config.yml info into the actual file (this required alot of shenanigans, as the API request normally gets it un-fucking formatting, which does not work, and well, IT BITCHES). Also max is gay ;)

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
+  GNU nano 4.8                                   test.sh                                    Modified
 #!/bin/bash
-read -p $'Press 0 for Node Deployment or Press 1 for Wings Install&Config\nRun Node Deployment then Wings Installation if you are using this to easily boot up servers\n' CHECK
-if [ $CHECK = 0 ]
+read -p $'Press 0 for Node Deployment or Press 1 for Wings Install&Config\nRun Node Deployment then W>if [ $CHECK = 0 ]
 then
 read -p "Please enter the domain of the panel: " domain
 read -p "Please enter the name of the Node: " name
@@ -9,6 +9,9 @@ read -p "Please enter a FQDN: " fqdn
 read -p "Please enter RAM Amount for the Node: " mem
 read -p "Pleae enter Disk Size: " disk
 read -p "Please enter the directory to the mounted disk: " mnt_disk
+read -p "Please enter the Upload Limit to the Node: " us
+read -p "Please enter the Port of the SFTP Service: " daemonsftp
+read -p "Please enter the Port of the Daemon (Wings): " daemonls
 JSONBody="{\
     \"domain\": \"${domain}\",\
     \"name\": \"${name}\",\
@@ -19,18 +22,24 @@ JSONBody="{\
     \"memory_overallocate\": 0,
     \"disk\": $disk,
     \"disk_overallocate\": 0,
-    \"upload_size\": 100,
-    \"daemon_sftp\": 2022,
-    \"daemon_listen\": 8080,
+    \"upload_size\": $us,
+    \"daemon_sftp\": $daemonsftp,
+    \"daemon_listen\": $daemonls,
     \"daemon_base\":  \"$mnt_disk\"
 }"
-curl "https://"${domain}"/api/application/nodes" \ | json_pp
+curl "https://"${domain}"/api/application/nodes" \
         -H 'Accept: application/json' \
         -H 'Content-Type: application/json' \
         -H "Authorization: Bearer KQe8z8jgxxRUrgSN7BoZdhJT1Dhu7loWsED6lSdTKUrHDbXx" \
         -X POST \
-        -d "$(jq -r . <<< "${JSONBody}")"
+        -d "$(jq -r . <<< "${JSONBody}")" | json_pp
 
+ufw allow 25565:25590/tcp
+ufw allow 25565:25590/udp
+ufw allow $daemonsftp
+ufw allow $daemonls
+echo "Ports: "{$daemonsftp}" , "{$daemonls}" and ports 25565-25590 have been allowed thru the firewall" 
+sleep 2
 echo "Node Has been Deployed to the Panel, Please run the Wings Install Part of this Script!"
 fi
 if [ $CHECK = 1 ]
@@ -41,13 +50,13 @@ curl -sSL https://get.docker.com/ | CHANNEL=stable bash
 systemctl enable --now docker
 mkdir -p /etc/pterodactyl
 curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
+echo "Max is gay ;)"
 chmod u+x /usr/local/bin/wings
 echo '[Unit]
 Description=Pterodactyl Wings Daemon
 After=docker.service
 Requires=docker.service
 PartOf=docker.service
-
 [Service]
 User=root
 WorkingDirectory=/etc/pterodactyl
@@ -58,9 +67,22 @@ Restart=on-failure
 StartLimitInterval=180
 StartLimitBurst=30
 RestartSec=5s
-
 [Install]
 WantedBy=multi-user.target' >  /etc/systemd/system/wings.service
+cd /etc/pterodactyl
+FILE=/etc/pterodactyl/config.yml
+if [ -f "$FILE" ]; then
+    rm config.yml
+    echo "Config File already exists, deleting file"
+fi
+read -p "Please Enter Node ID, which can be found on the admin panel by clicking on the node and looking at the URL: " node_id
+curl "https://panel.crewcraft.cc/api/application/nodes/"${node_id}"/configuration" \
+  -H 'Accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -H 'Authorization: Bearer KQe8z8jgxxRUrgSN7BoZdhJT1Dhu7loWsED6lSdTKUrHDbXx' \
+  -X GET  | json_pp >> config.yml
 systemctl enable --now wings
-echo "Wings have been installed, please auto-deploy the node with the command given with Pterodactyl!"
+systemctl restart wings
+echo "Wings have been installed and configured"
+cd /
 fi
