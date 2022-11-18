b2edee371045db0d63d7fa750007b2d30275bb41
TheYeetster <64783847+Yeetster-cmd@users.noreply.github.com>
2022-11-18 10:34:17 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -49,6 +49,9 @@ apt install curl
 #WIP, will be replaced later on
 curl -sSL https://get.docker.com/ | CHANNEL=stable bash
 systemctl enable --now docker
+mkdir -p /etc/pterodactyl
+curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
+chmod u+x /usr/local/bin/wings
 
 sleep 180
 cd /etc/pterodactyl
@@ -66,7 +69,29 @@ curl "https://"${p_domain}"/api/application/nodes/"${node_id}"/configuration" \
   -H 'Authorization: Bearer $apikey' \
   -X GET  | json_pp >> config.yml
 systemctl enable --now wings
+cd  /etc/systemd/system
+echo "[Unit]
+Description=Pterodactyl Wings Daemon
+After=docker.service
+Requires=docker.service
+PartOf=docker.service
+
+[Service]
+User=root
+WorkingDirectory=/etc/pterodactyl
+LimitNOFILE=4096
+PIDFile=/var/run/wings/daemon.pid
+ExecStart=/usr/local/bin/wings
+Restart=on-failure
+StartLimitInterval=180
+StartLimitBurst=30
+RestartSec=5s
+
+[Install]
+WantedBy=multi-user.target" > wings.service
+systemctl enable --now wings
 systemctl restart wings
+
 echo "Wings have been installed and configured"
 cd /
 fi
