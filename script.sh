83989df39460223cf125f72f7184fe76d900c5e4
TheYeetster <64783847+Yeetster-cmd@users.noreply.github.com>
2022-11-23 16:25:40 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -53,7 +53,6 @@ mkdir -p /etc/pterodactyl
 curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
 chmod u+x /usr/local/bin/wings
 
-sleep 180
 cd /etc/pterodactyl
 FILE=/etc/pterodactyl/config.yml
 if [ -f "$FILE" ]; then
